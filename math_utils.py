f776cfae63ae1a5d207f08ef54201a335190b689
İbrahim Ege Kırtık <95716791+Egekrtk@users.noreply.github.com>
2025-04-10 03:27:22 -0700

    math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,12 @@
+def add(x,y):
+        return x+y;
+def substract(x,y):
+        return x-y;
+def multiply(x,y):
+        return x*y;
+def divide(x,y):
+        return x /y;
+def power(x,y):
+        return x**y;
+def mod(x,y):
+        return x%y;
